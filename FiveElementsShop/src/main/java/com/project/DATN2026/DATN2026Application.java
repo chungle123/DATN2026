306a508f08d8eada679b16dc4a527755f4942dff
chungle123 <chungbeole07@gmail.com>
2026-03-20 11:32:47 +0700

    add entity Account dto

---
+++ FiveElementsShop/src/main/java/com/project/DATN2026/DATN2026Application.java
@@ -0,0 +1,13 @@
+package com.project.DATN2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DATN2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DATN2026Application.class, args);
+	}
+
+}
---
+++ FiveElementsShop/src/main/java/com/project/DATN2026/dto/Account/AccountDto.java
@@ -0,0 +1,15 @@
+package com.project.DATN2026.dto.Account;
+
+import com.project.DATN2026.dto.AddressShipping.AddressShippingDto;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class AccountDto {
+    private String phoneNumber;
+    private String name;
+    private String email;
+    private String password;
+    private List<AddressShippingDto> addressShippingList;
+}
