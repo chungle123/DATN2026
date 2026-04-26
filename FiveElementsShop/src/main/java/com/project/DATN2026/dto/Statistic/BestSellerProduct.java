cbb087bd96ffa1a61bf45bbae1c11b63bb378091
chungle123 <chungbeole07@gmail.com>
2026-04-26 11:42:32 +0700

    add bootrap css

---
+++ FiveElementsShop/src/main/java/com/project/DATN2026/dto/Statistic/BestSellerProduct.java
@@ -0,0 +1,10 @@
+package com.project.DATN2026.dto.Statistic;
+
+public interface BestSellerProduct {
+    Long getId();
+    String getCode();
+    String getName();
+    String getImageUrl();
+    String getTotalQuantity();
+    String getRevenue();
+}
